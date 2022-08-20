a42042776891e2b89925d471d0568777c1f033c7
Daniel Matsumoto <daniel.matsumoto@ifood.com.br>
2022-08-20 04:09:58 -0300

    fixed program printing <nil> on unsuccessful runs

--- thankunext.go
+++ thankunext.go
@@ -9,15 +9,21 @@ import (
 )
 
 func dumpBuildManifestData(url string) string {
+	val := ""
 	if strings.HasSuffix(url, "buildManifest.js") {
 		page := rod.New().MustConnect().MustPage(url)
 		page.MustWaitLoad().MustEval("() => {eval(document.documentElement.innerText)}")
-		val := page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
-		return val
+		val = page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+
 	} else {
 		page := rod.New().MustConnect().MustPage(url)
-		val := page.MustWaitLoad().MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+		val = page.MustWaitLoad().MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+
+	}
+	if val != "<nil>" {
 		return val
+	} else {
+		return ""
 	}
 
 }
@@ -25,7 +31,8 @@ func dumpBuildManifestData(url string) string {
 func main() {
 	fmt.Fprintln(os.Stderr, `<!-- thankunext v0.01, made by @c3l3si4n -->`)
 	if len(os.Args[1:]) > 0 {
-		fmt.Println(dumpBuildManifestData(os.Args[1]))
+
+		fmt.Printf("%s", dumpBuildManifestData(os.Args[1]))
 	} else {
 		fmt.Printf("[thankunext] %s <_buildManifest.js url>", os.Args[0])
 	}

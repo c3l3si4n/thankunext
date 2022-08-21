8346c1d00e9f1509c7235370d87e6430e0fc7c8f
Daniel Matsumoto <daniel.matsumoto@ifood.com.br>
2022-08-20 22:43:07 -0300

    added timeout operation

--- thankunext.go
+++ thankunext.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/go-rod/rod"
 )
 
+func handleError(err error) {
+	var evalErr *rod.ErrEval
+	if errors.Is(err, context.DeadlineExceeded) { // timeout error
+		fmt.Println("timeout err")
+	} else if errors.As(err, &evalErr) { // eval error
+		fmt.Println(evalErr.LineNumber)
+	} else if err != nil {
+		fmt.Println("can't handle", err)
+	}
+}
+
 func dumpBuildManifestData(url string) string {
 	val := ""
 	if strings.HasSuffix(url, "buildManifest.js") {
-		page := rod.New().MustConnect().MustPage(url)
-		page.MustWaitLoad().MustEval("() => {eval(document.documentElement.innerText)}")
-		val = page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+		page := rod.New().MustConnect()
+		err := rod.Try(func() {
+			loaded_page := page.Timeout(10 * time.Second).MustPage(url).MustWaitLoad()
+			loaded_page.MustEval("() => eval(document.documentElement.innerText)")
+			val = loaded_page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+		})
+
+		if err != nil {
+			return ""
+		}
 
 	} else {
-		page := rod.New().MustConnect().MustPage(url)
-		val = page.MustWaitLoad().MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+		page := rod.New().MustConnect()
+		err := rod.Try(func() {
+			loaded_page := page.Timeout(10 * time.Second).MustPage(url).MustWaitLoad()
+			val = loaded_page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+		})
 
+		if err != nil {
+			return ""
+		}
 	}
+
 	if val != "<nil>" {
 		return val
 	} else {

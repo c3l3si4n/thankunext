68c265c75faf316d81ad0c3b00ae2612c49f2eac
Guilherme <constshi0n@proton.me>
2022-10-10 20:26:08 -0300

    feat: added parsing with regex

--- thankunext.go
+++ thankunext.go
@@ -1,95 +1,120 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/url"
+	"io"
+	"net/http"
 	"os"
+	"regexp"
 	"strings"
-	"time"
-
-	"github.com/go-rod/rod"
 )
 
-func handleError(err error) {
-
-	var evalErr *rod.ErrEval
-
-	if errors.Is(err, context.DeadlineExceeded) { // timeout error
-		fmt.Println("timeout err")
-	} else if errors.As(err, &evalErr) { // eval error
-		fmt.Println(evalErr.LineNumber)
-	} else if err != nil {
-		fmt.Println("can't handle", err)
+func contains(elements []string, element string) bool {
+	for _, v := range elements {
+		if v == element {
+			return true
+		}
 	}
+
+	return false
 }
 
-func dumpBuildManifestData(input_url string) string {
-	val := ""
+func getBuildManifestPath(bodyContent string) string {
+	re, _ := regexp.Compile(`(?m)/_next/static/[\w-]+/_buildManifest\.js`)
 
-	type buildManifestStruct struct {
-		SortedPages []string `json:sortedPages`
-	}
+	return re.FindString(bodyContent)
+}
 
-	if strings.HasSuffix(input_url, "buildManifest.js") {
-		page := rod.New()
-		err := rod.Try(func() {
-			loaded_page := page.MustConnect().Timeout(30 * time.Second).MustPage(input_url).MustWaitLoad()
-			loaded_page.MustEval("() => eval(document.documentElement.innerText)")
-			val = loaded_page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
+func getPageContent(url string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
 
-		})
-
-		if err != nil {
-			fmt.Println("Error on loading rod browser.", err)
-			return ""
-		}
-
-	} else {
-		page := rod.New()
-		err := rod.Try(func() {
-			loaded_page := page.MustConnect().Timeout(30 * time.Second).MustPage(input_url).MustWaitLoad()
-			val = loaded_page.MustEval("() => JSON.stringify(self.__BUILD_MANIFEST)").Str()
-		})
-
-		if err != nil {
-			fmt.Println("Error on loading rod browser.", err)
-			return ""
-		}
-	}
-
-	if val != "<nil>" {
-		var buildManifestUnmarshal buildManifestStruct
-		if err := json.Unmarshal([]byte(val), &buildManifestUnmarshal); err != nil {
-			fmt.Fprintf(os.Stderr, "%v", err)
-			return ""
-		}
-		sortedPages := buildManifestUnmarshal.SortedPages
-		u, _ := url.Parse(input_url)
-		u.RawQuery = ""
-		u.Fragment = ""
-		u.Path = ""
-		for _, element := range sortedPages {
-			u_new := u
-			u_new.Path = element
-			fmt.Printf("%s\n", u_new.String())
-		}
-		return val
-	} else {
+	if err != nil {
+		fmt.Println("Error on NewRequest.", err)
 		return ""
 	}
+
+	req.Header.Add("user-agent", "thankunext/1.0")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Error accessing url.", err)
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("Error loading page content.", err)
+		return ""
+	}
+
+	return string(body)
+}
+
+func getBuildManifestContent(buildManifestPath string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", buildManifestPath, nil)
+
+	if err != nil {
+		fmt.Println("Error on NewRequest.", err)
+		return ""
+	}
+
+	req.Header.Add("user-agent", "thankunext/1.0")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Error accessing buildManifest content.", err)
+		return ""
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("Error loading buildManifest contents.", err)
+		return ""
+	}
+
+	return string(body)
+
+}
+
+func parseBuildManifestContent(buildManifestContent string) []string {
+	re, _ := regexp.Compile(`"(/[a-zA-Z0-9_/\[\]\.-]+)"`)
+	var paths []string
+
+	for _, match := range re.FindAllStringSubmatch(buildManifestContent, -1) {
+		path := match[1]
+
+		if contains(paths, path) {
+			continue
+		}
+
+		paths = append(paths, path)
+	}
+
+	return paths
 
 }
 
 func main() {
 	fmt.Fprintln(os.Stderr, `<!-- thankunext v0.01, made by @c3l3si4n -->`)
-	if len(os.Args[1:]) > 0 {
 
-		dumpBuildManifestData(os.Args[1])
+	if len(os.Args[1:]) > 0 {
+		target := os.Args[1]
+		pageContent := getPageContent(target)
+		buildManifestPath := getBuildManifestPath(pageContent)
+		buildManifestContent := getBuildManifestContent(target + buildManifestPath)
+		paths := parseBuildManifestContent(buildManifestContent)
+
+		fmt.Println(strings.Join(paths, "\n"))
+
 	} else {
-		fmt.Printf("[thankunext] %s <_buildManifest.js url>", os.Args[0])
+		fmt.Printf("[thankunext] %s <url>", os.Args[0])
 	}
-	//buildManifestUrl := os.Args
 }
